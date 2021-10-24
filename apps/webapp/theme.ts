4b8b59e1f4388a44ece519be5a970b4cf8ceabf4
Akos Szalay <akos.szalay@gmail.com>
2021-10-24 16:23:53 +0100

    switched to SPA from nextjs fro UI, setup routing/i18n and basic call to API

--- apps/webapp/theme.ts
+++
@@ -1,7 +0,0 @@
-import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
-
-// Create a theme instance.
-const theme = createTheme();
-
-export default theme;
